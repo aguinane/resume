f9c652d2fc03f9d04d9aa533e20b4e4f171dedfb
Alex Guinman <alex@guinman.id.au>
2020-04-02 16:55:17 +1000

    Download from gist and formatting fixes

--- run.py
+++ run.py
@@ -5,16 +5,22 @@ import json
 import pdfkit
 from jinja2 import Environment, FileSystemLoader
 import colander
+from dateutil.parser import parse
 from jsonresume.schema.resume import Resume as ResumeSchema
-
+import requests
 
 env = Environment(loader=FileSystemLoader("templates"))
 
 
 def main():
-    json_file = open("data/resume.json", "r")
-    resume_data = json.load(json_file)
+    # resume_data = json.load(open("data/resume.json", "r"))
+    resume_data = download_json("aguinane", "5dc4e0eec034077b58fcf44add8e13c9")
+    build_resume(resume_data)
+
+
+def build_resume(resume_data: dict):
     validate_json_format(resume_data)
+    env.filters["yearmonth"] = format_month
     template = env.get_template("resume.html")
     output_html = template.render(**resume_data)
     Path("build").mkdir(exist_ok=True)
@@ -23,6 +29,25 @@ def main():
     copy_static_data()
     logging.info("Created html!")
     pdfkit.from_file("build/resume.html", "build/resume.pdf")
+
+
+def download_json(user: str, gist_hash: str):
+    """ Download resume.json github gist file"""
+    filename = "resume.json"
+    url = f"https://gist.githubusercontent.com/{user}/{gist_hash}/raw/{filename}"
+    logging.info("Downloading %s", url)
+    r = requests.get(url)
+    r.raise_for_status()
+    return r.json()
+
+
+def format_month(value: str) -> str:
+    try:
+        dt = parse(value)
+    except ValueError:
+        return value
+    month_desc = dt.strftime("%b %Y")
+    return month_desc
 
 
 def validate_json_format(resume_data):
